4dc0921ab7027ed1acddeed81a058cc5fa214d34
Dev02Liu <mail@yijie-liu.com>
2024-05-19 19:15:06 +0200

    added create job page

--- job-portal-frontend/src/app/app.module.ts
+++ job-portal-frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 // Import necessary Angular modules and components
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -20,7 +21,8 @@ import { JobFormComponent } from './components/job-form/job-form.component';
     // Import necessary modules
     HttpClientModule, // Module to enable HTTP communication
     BrowserModule, // Module required to run the app in a browser
-    AppRoutingModule // Module for application routing
+    AppRoutingModule, // Module for application routing
+    FormsModule // Module for Angular Forms
   ],
   providers: [],
   bootstrap: [AppComponent] // Root component to bootstrap the application
