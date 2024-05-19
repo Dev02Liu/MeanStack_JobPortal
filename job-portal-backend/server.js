637a90a3c704c6306c171e4247337fc87ac39079
Dev02Liu <mail@yijie-liu.com>
2024-05-20 00:29:13 +0200

    removed body-parser

--- job-portal-backend/server.js
+++ job-portal-backend/server.js
@@ -1,7 +1,6 @@
 // Import required modules
 const express = require('express'); // Express framework for building web applications
 const mongoose = require('mongoose'); // Mongoose library for MongoDB object modeling
-const bodyParser = require('body-parser'); // Middleware to parse request bodies
 const cors = require('cors'); // Middleware to enable Cross-Origin Resource Sharing (CORS)
 
 // Create an instance of an Express application
@@ -13,8 +12,9 @@ mongoose.connect('mongodb://localhost:27017/jobportal');
 // Use CORS middleware to allow cross-origin requests
 app.use(cors());
 
-// Use bodyParser middleware to parse JSON request bodies
-app.use(bodyParser.json());
+// Use built-in Express JSON parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Import job routes
 const jobRoutes = require('./routes/jobRoutes'); // Routes for job-related API endpoints
